a899beafd92aa2133e193b6851e0f9d3c2e5da66
Kristopher Lea <Kristopherlea187@gmail.com>
2020-05-30 15:22:35 -0500

    added User MVC

---
+++ server-java-mysql/src/main/java/com/example/groupproject/models/User.java
@@ -0,0 +1,82 @@
+package com.example.groupproject.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userid;
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String role;
+	private String gradelevel;
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getGradelevel() {
+		return gradelevel;
+	}
+
+	public void setGradelevel(String gradelevel) {
+		this.gradelevel = gradelevel;
+	}
+
+	@Column(columnDefinition = "boolean default false")
+	private boolean complete;
+
+	public boolean getComplete() {
+		return this.complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+}
