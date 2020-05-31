01433302a35debdcc4493d0c2c893edd10fa6069
Kristopher Lea <Kristopherlea187@gmail.com>
2020-05-30 22:19:09 -0500

    .

--- server-java-mysql/src/main/java/com/example/groupproject/GroupProjectApplication.java
+++ server-java-mysql/src/main/java/com/example/groupproject/GroupProjectApplication.java
@@ -1,14 +1,12 @@
-package com.example.groupproject.auth;
+package com.example.groupproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.example.groupproject"})
 public class GroupProjectApplication {
 
 	public static void main(String[] args) {
