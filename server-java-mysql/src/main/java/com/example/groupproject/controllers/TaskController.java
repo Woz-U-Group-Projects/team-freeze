0080839b4d1eb60d4bcc5d648f7250bbc324e1bc
Kristopher Lea <Kristopherlea187@gmail.com>
2020-05-30 22:08:46 -0500

    deleted security

--- server-java-mysql/src/main/java/com/example/groupproject/controllers/TaskController.java
+++ server-java-mysql/src/main/java/com/example/groupproject/controllers/TaskController.java
@@ -1,4 +1,4 @@
-package com.example.groupproject.auth;
+package com.example.groupproject.controllers;
 
 import java.util.List;
 
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.groupproject.models.Task;
+import com.example.groupproject.models.TaskRepository;
 
 @RestController
 @RequestMapping("/tasks")
